c5f494ddb7fdce892ba21f0596dc20786a7edb46
Richard Brown <rgbrown@gmail.com>
2022-10-17 16:38:01 +1300

    Starting three body problem animation

---
+++ threebody.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env/python3
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from scipy.integrate import odeint
+
+# Three body problem animation
+m1, m2, m3 = 1
+G = 1
+
+def f(u, t):
+    pass
+
+
+
